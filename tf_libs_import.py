3d2516969d90eb69c34680e7fb6db3dfa681914d
TomFarley <farleytpm@gmail.com>
2015-08-24 21:08:19 +0100

    Converted with 2to3. Python2 files in python2 folder. Import behaviour correct.

--- tf_libs_import.py
+++ tf_libs_import.py
@@ -1,4 +1,4 @@
 #import tf_libs as tf
-import tf_debug as debug
-import tf_numeric as numeric
+from . import tf_debug as debug
+from . import tf_numeric as numeric
 #import tf_debug.tf_debug as tf_debug
